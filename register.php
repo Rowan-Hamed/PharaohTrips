017caaf9a855fe8f8915ec8615d5b2e30d6753c8
Rowan-Hamed <132090103+Rowan-Hamed@users.noreply.github.com>
2023-05-07 22:48:18 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,24 +1,12 @@
-  <!-- isset($_POST['sign up']) -->
-  <?php
+ <?php
 if ( ($_SERVER['REQUEST_METHOD'] == 'POST')) {
     $data = json_decode(file_get_contents('users.json'), true);
 
-    // Get form data
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $password2 = password_hash($_POST['password2'], PASSWORD_DEFAULT);
 
-    // if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    //     echo '<center><h1>Invalid email address</h1></cenetr>';
-     ?>
-    <!-- // <center><button><a href="regpage.html">Back</a></button></center> -->
-        <?php
-    //     exit();
-    // }
-    
-
-    // Check if email already exists
     foreach ($data as $user) {
         if ($user['email'] == $email) {
             echo '<center><h1>Email already exists</h1></center>';
@@ -29,21 +17,25 @@ if ( ($_SERVER['REQUEST_METHOD'] == 'POST')) {
         }
     }
     
-    // Add new user to data array
     $data[] = array(
         'username' => $username,
         'email' => $email,
         'password' => $password
     );
+
+    foreach ($data as $key => $item) {
+        $data[$key]['id'] = uniqid();
+    }
     
-    // Save data to JSON file
     file_put_contents('users.json', json_encode($data));
 
     echo 'Registration successful!';
-    // Redirect user to login page
+
     header('Location:LoginPage.html');
-    exit();
-  
-    
+    exit();   
 }
 ?>
+
+
+
+
